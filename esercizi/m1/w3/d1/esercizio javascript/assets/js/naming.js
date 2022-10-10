8887edd498512d2242c305a2d1b19ff7a4c45ce4
Maurizio Villanova <113591360+MaurizioVillanova@users.noreply.github.com>
2022-10-10 11:17:25 +0200

    esercizio javascript 1

---
+++ esercizi/m1/w3/d1/esercizio javascript/assets/js/naming.js
@@ -0,0 +1,21 @@
+var pet_preferito = 'gatto';
+console.log(pet_preferito);
+
+var pet = 'gatto';
+document.getElementById('animale').innerHTML =pet;
+var Pet = 'cane';
+console.log(Pet);
+var petPreferito = 'criceto';
+var PetPreferito = 'coniglio';
+
+var $pet = 'giraffa';
+console.log($pet);
+var _pet = 'leone';
+console.log(_pet);
+var _pet2 = 'pantera';
+document.write(_pet2);
+
+var numero = 12;
+var numero2 = 14;
+var risultato = numero + numero2;
+document.getElementById('calcolo').innerHTML = risultato;
