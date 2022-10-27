e4317f12a737ede36640821ac5e7b7e14519d573
Maurizio Villanova <113591360+MaurizioVillanova@users.noreply.github.com>
2022-10-27 12:51:06 +0200

    commit todolist

---
+++ esercizi/m2/w1/d4/assets/js/script.js
@@ -0,0 +1,37 @@
+function toggleMenu() {
+  document.getElementById("btn-panel").classList.toggle("mostra");
+}
+
+document.querySelector("#btn1").addEventListener("click", function () {
+  document.querySelector(".cambia").style.fontSize = "40px";
+});
+document.querySelector("#btn2").addEventListener("click", function  () {
+  document.querySelector(".cambia").style.color = "red";
+});
+document.querySelector("#btn3").addEventListener("click", function () {
+  document.querySelector(".cambia").style.textTransform = "uppercase";
+});
+document.querySelector("#btn4").addEventListener("click", function () {
+  document.querySelector(".cambia").style.visibility = "hidden";
+});
+document.querySelector("#btn5").addEventListener("click", function () {
+  document.querySelector(".cambia").style.visibility = "visible";
+});
+
+
+
+
+
+//gestione della lista
+let lista = document.querySelectorAll('li');
+for (var i = 0; i < lista.length; i++) {
+    lista[i].addEventListener('mouseover', function() {
+        this.classList.add('selezione');
+    });
+    lista[i].addEventListener('mouseout', function() {
+        this.classList.remove('selezione');
+    });
+    lista[i].addEventListener('click', function() {
+        this.classList.toggle('fatto');
+    });
+}
