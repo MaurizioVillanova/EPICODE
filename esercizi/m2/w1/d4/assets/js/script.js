975ecb646fcb85f60165a90ab509a055c34cb310
Maurizio Villanova <113591360+MaurizioVillanova@users.noreply.github.com>
2022-10-31 11:41:33 +0100

    commit bootstrap

--- esercizi/m2/w1/d4/assets/js/script.js
+++ esercizi/m2/w1/d4/assets/js/script.js
@@ -25,12 +25,6 @@ document.querySelector("#btn5").addEventListener("click", function () {
 //gestione della lista
 let lista = document.querySelectorAll('li');
 for (var i = 0; i < lista.length; i++) {
-    lista[i].addEventListener('mouseover', function() {
-        this.classList.add('selezione');
-    });
-    lista[i].addEventListener('mouseout', function() {
-        this.classList.remove('selezione');
-    });
     lista[i].addEventListener('click', function() {
         this.classList.toggle('fatto');
     });
