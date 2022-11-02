9162bbecdc505be72ec82a6d586b79501fdf5d28
Maurizio Villanova <113591360+MaurizioVillanova@users.noreply.github.com>
2022-11-02 11:02:21 +0100

    commit bootstrap2

--- esercizi/m2/w2/d1/to do list 2/assets/js/script.js
+++ esercizi/m2/w2/d1/to do list 2/assets/js/script.js
@@ -1,10 +1,20 @@
-var btn = document.querySelector('#inserisci');
-var titolo = document.getElementById('titolo').value;
-var compito = document.getElementById('compito').value;
-var todo = titolo + '' + compito;
-var lista = document.getElementById('lista');
-btn.addEventListener('click', function(){
-    var li = document.createElement('li');
-    var text = document.createTextNode(todo);
-    lista.appendChild(li)
-})
+var btn = document.getElementById("inserisci");
+btn.addEventListener("click", function () {
+  var titolo = document.getElementById("titolo").value;
+  var compito = document.getElementById("compito").value;
+  var todo = titolo + " " + compito;
+  var lista = document.getElementById("lista");
+  var li = document.createElement("li");
+  var text = document.createTextNode(todo);
+  lista.appendChild(li);
+  li.appendChild(text);
+  const buttonDelete = document.createElement('button');
+  buttonDelete.className = 'delete';
+  buttonDelete.appendChild(document.createTextNode('X'));    
+  li.appendChild(buttonDelete);
+  buttonDelete.addEventListener('click', (event) =>{
+    const parentNodeEl = event.target.parentNode;
+    setTimeout(() =>{
+        parentNodeEl.remove();
+    }, 500)})
+});
