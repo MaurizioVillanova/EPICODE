731382c0fe59533adbb7a6bed2f540fd4d5977d0
Maurizio Villanova <113591360+MaurizioVillanova@users.noreply.github.com>
2022-11-30 12:09:35 +0100

    ang3

---
+++ esercizi/m3/w2/d3/src/app/components/post-card/post-card.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-card',
+  template: `
+    <p>
+      post-card works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class PostCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
